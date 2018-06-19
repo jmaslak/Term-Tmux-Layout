c19710457ffb2183055840cbc73462e279c0109a
Joelle Maslak <jmaslak@antelope.net>
2018-06-19 10:50:38 -0600

    Add /a regex monitor when using \d

--- lib/Term/Tmux/Layout.pm
+++ lib/Term/Tmux/Layout.pm
@@ -447,7 +447,7 @@ sub get_window_size {
     @windows = grep { /\(active\)$/ } map { chomp; $_ } @windows;
 
     if (scalar(@windows)) {
-        my ($x, $y) = $windows[0] =~ / \[(\d+)x(\d+)\] /;
+        my ($x, $y) = $windows[0] =~ / \[(\d+)x(\d+)\] /a;
         return ($x, $y);
     }
 
